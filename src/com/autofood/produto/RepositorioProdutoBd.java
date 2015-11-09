052d87fa7161bc8645e826562224b75a5fe94dab
Vinicius <lindemberg.vinicius@gmail.com>
2015-11-08 23:08:02 -0300

    Correçoes

--- src/com/autofood/produto/RepositorioProdutoBd.java
+++ src/com/autofood/produto/RepositorioProdutoBd.java
@@ -1,22 +1,27 @@
 package com.autofood.produto;
 
 import java.util.ArrayList;
+import com.autofood.exceçõesProduto.NomeVazioException;
+import com.autofood.exceçõesProduto.ProdutoJáCadastradoException;
+import com.autofood.exceçõesProduto.ProdutoNaoEncontradoException;
 
 public class RepositorioProdutoBd implements IRepositorioProduto {
 
-	public void cadastra(Produto produto) {
+	public void cadastra(Produto produto) throws ProdutoJáCadastradoException,
+			NomeVazioException {
 
 	}
 
-	public void atualizar(Produto produto) {
+	public void atualizar(Produto produto) throws ProdutoNaoEncontradoException {
 
 	}
 
-	public void remover(Integer idProduto) {
+	public void remover(Integer idProduto) throws ProdutoNaoEncontradoException {
 
 	}
 
-	public Produto procurar(Integer idProduto) {
+	public Produto procurar(Integer idProduto)
+			throws ProdutoNaoEncontradoException {
 
 		return null;
 	}
