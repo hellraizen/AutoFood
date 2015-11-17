dfc719edd1193cd2aaf3f1fafb501b7a3d029b4f
Vinicius <lindemberg.vinicius@gmail.com>
2015-11-16 21:07:44 -0300

    çkhjn

---
+++ src/com/autofood/telas/JFEstoque.java
@@ -0,0 +1,182 @@
+package com.autofood.telas;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.SystemColor;
+import javax.swing.border.TitledBorder;
+import javax.swing.UIManager;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class JFEstoque extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtQuantidade;
+	private JTextField txtPreco;
+	private JTextField txtValidade;
+	private JTextField txtProduto;
+	private JTextField txtCodigo;
+	private JTextField txtDataEntrada;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFEstoque frame = new JFEstoque();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JFEstoque() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 652, 512);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JPanel panelCadastro = new JPanel();
+		panelCadastro.setToolTipText("");
+		panelCadastro.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Cadastro Produtos",
+						TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panelCadastro.setBackground(SystemColor.menu);
+		
+		txtQuantidade = new JTextField();
+		txtQuantidade.setColumns(10);
+		
+		JLabel lblQuantidade = new JLabel("Quantidade\r\n");
+		
+		JLabel lblPreco = new JLabel("Pre\u00E7o");
+		
+		txtPreco = new JTextField();
+		txtPreco.setColumns(10);
+		
+		JLabel lblValidade = new JLabel("Validade");
+		
+		txtValidade = new JTextField();
+		txtValidade.setColumns(10);
+		
+		txtProduto = new JTextField();
+		txtProduto.setColumns(10);
+		
+		JLabel lblProduto = new JLabel("Produto");
+		
+		JLabel lblCodigo = new JLabel("Codigo:");
+		
+		txtCodigo = new JTextField();
+		txtCodigo.setColumns(10);
+		
+		txtDataEntrada = new JTextField();
+		txtDataEntrada.setColumns(10);
+		
+		JLabel lblDataEntrada = new JLabel("Data Entrada");
+		GroupLayout gl_panelCadastro = new GroupLayout(panelCadastro);
+		gl_panelCadastro.setHorizontalGroup(
+			gl_panelCadastro.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 605, Short.MAX_VALUE)
+				.addGroup(gl_panelCadastro.createSequentialGroup()
+					.addGap(4)
+					.addGroup(gl_panelCadastro.createParallelGroup(Alignment.LEADING, false)
+						.addGroup(gl_panelCadastro.createSequentialGroup()
+							.addGroup(gl_panelCadastro.createParallelGroup(Alignment.LEADING)
+								.addComponent(txtQuantidade, GroupLayout.PREFERRED_SIZE, 154, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblQuantidade, GroupLayout.PREFERRED_SIZE, 71, GroupLayout.PREFERRED_SIZE))
+							.addGap(18)
+							.addGroup(gl_panelCadastro.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(lblPreco, GroupLayout.PREFERRED_SIZE, 118, GroupLayout.PREFERRED_SIZE)
+								.addComponent(txtPreco, GroupLayout.PREFERRED_SIZE, 118, GroupLayout.PREFERRED_SIZE))
+							.addGap(36)
+							.addGroup(gl_panelCadastro.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblValidade, GroupLayout.PREFERRED_SIZE, 71, GroupLayout.PREFERRED_SIZE)
+								.addComponent(txtValidade, GroupLayout.PREFERRED_SIZE, 197, GroupLayout.PREFERRED_SIZE)))
+						.addGroup(gl_panelCadastro.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_panelCadastro.createParallelGroup(Alignment.LEADING)
+								.addComponent(txtProduto, GroupLayout.PREFERRED_SIZE, 335, GroupLayout.PREFERRED_SIZE)
+								.addGroup(gl_panelCadastro.createSequentialGroup()
+									.addGroup(gl_panelCadastro.createParallelGroup(Alignment.TRAILING)
+										.addGroup(gl_panelCadastro.createSequentialGroup()
+											.addComponent(lblProduto, GroupLayout.PREFERRED_SIZE, 71, GroupLayout.PREFERRED_SIZE)
+											.addGap(91))
+										.addGroup(gl_panelCadastro.createSequentialGroup()
+											.addComponent(lblCodigo)
+											.addPreferredGap(ComponentPlacement.UNRELATED)))
+									.addComponent(txtCodigo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addGroup(gl_panelCadastro.createParallelGroup(Alignment.LEADING)
+								.addComponent(txtDataEntrada)
+								.addComponent(lblDataEntrada, GroupLayout.PREFERRED_SIZE, 136, GroupLayout.PREFERRED_SIZE))))
+					.addContainerGap(66, Short.MAX_VALUE))
+		);
+		gl_panelCadastro.setVerticalGroup(
+			gl_panelCadastro.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 164, Short.MAX_VALUE)
+				.addGroup(gl_panelCadastro.createSequentialGroup()
+					.addGroup(gl_panelCadastro.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panelCadastro.createSequentialGroup()
+							.addGap(28)
+							.addGroup(gl_panelCadastro.createParallelGroup(Alignment.TRAILING)
+								.addComponent(lblProduto)
+								.addComponent(lblDataEntrada)))
+						.addGroup(gl_panelCadastro.createSequentialGroup()
+							.addContainerGap()
+							.addGroup(gl_panelCadastro.createParallelGroup(Alignment.BASELINE)
+								.addComponent(txtCodigo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblCodigo))))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panelCadastro.createParallelGroup(Alignment.BASELINE)
+						.addComponent(txtProduto, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtDataEntrada, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_panelCadastro.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_panelCadastro.createSequentialGroup()
+							.addComponent(lblQuantidade)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(txtQuantidade, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panelCadastro.createSequentialGroup()
+							.addGroup(gl_panelCadastro.createParallelGroup(Alignment.BASELINE)
+								.addComponent(lblPreco)
+								.addComponent(lblValidade))
+							.addGap(1)
+							.addGroup(gl_panelCadastro.createParallelGroup(Alignment.BASELINE)
+								.addComponent(txtPreco, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(txtValidade, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
+					.addGap(83))
+		);
+		panelCadastro.setLayout(gl_panelCadastro);
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(panelCadastro, GroupLayout.PREFERRED_SIZE, 605, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(11, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(panelCadastro, GroupLayout.PREFERRED_SIZE, 164, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(299, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
